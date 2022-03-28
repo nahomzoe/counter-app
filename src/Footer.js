77a0f415b276e71544876fe0c0d7aa9d7ee6e12b
Asfaw Nahom <s2200108@edu.bc.fi>
2022-03-28 15:21:55 +0300

    learn about how to use click handler in react and change function to class component

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div id="footer">
+        <p>Made by Nahom</p>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div id="logo">
+        <h2>Counter app</h2>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
